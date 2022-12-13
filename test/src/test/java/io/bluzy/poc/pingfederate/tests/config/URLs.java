0ba58b15d431192c3276167b743a909adb64f945
Florin Potera <fpotera@hotmail.com>
2022-12-13 17:02:56 +0100

    add more configuration

--- test/src/test/java/io/bluzy/poc/pingfederate/tests/config/URLs.java
+++ test/src/test/java/io/bluzy/poc/pingfederate/tests/config/URLs.java
@@ -25,4 +25,6 @@ public interface URLs {
 
     String PING_FED_IDP_ADAPTER_GRANT_MAPPING_URL = format("https://{0}:9999/pf-admin-api/v1/oauth/idpAdapterMappings", PING_FEDERATE_HOST);
 
+    String PING_FED_SERVER_SETTINGS_URL = format("https://{0}:9999/pf-admin-api/v1/serverSettings", PING_FEDERATE_HOST);
+
 }
